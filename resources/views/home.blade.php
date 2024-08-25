ebe894951c7f5af40f6c77872fe8d2f9841150fa
nisal-basura <21ug0795@sltc.ac.lk>
2024-08-25 22:40:39 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header bg-primary text-white text-center">
+                    <h4>{{ __('Dashboard') }}</h4>
+                </div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h5>{{ Auth::user()->name }}, you are successfully logged in!</h5>
+                    <p>Welcome to the system. Explore the options below:</p>
+                    
+                    <!-- Centering buttons -->
+                    <div class="mt-4">
+                        <a href="#" class="btn btn-success btn-lg mr-2">Profile</a>
+                        <a href="#" class="btn btn-info btn-lg mr-2">Settings</a>
+                        <a href="#" class="btn btn-danger btn-lg">Logout</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
